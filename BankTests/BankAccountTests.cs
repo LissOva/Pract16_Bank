2cf3b84f5f73751083a20a0af95e7612a74d1367
Колыгина Елизавета <liziks.sova@gmail.com>
2023-03-30 09:24:47 +0300

    +My tests

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -7,7 +7,7 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void Debit_WithValidAmount_UpdatesBalance()
+        public void Debit_WithValidAmount_UpdatesBalance() //Валидные значения
         {
             // Arrange
             double beginningBalance = 11.99;
@@ -22,7 +22,7 @@ namespace BankTests
         }
 
         [TestMethod]
-        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange() //Сумма по дебиту меньшу нуля
         {
             // Arrange
             double beginningBalance = 11.99;
@@ -34,7 +34,7 @@ namespace BankTests
         }
 
         [TestMethod]
-        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange() //Сумма по дебиту больше баланса
         {
             // Arrange
             double beginningBalance = 11.99;
@@ -53,6 +53,46 @@ namespace BankTests
             }
             Assert.Fail("The expected exception was not thrown.");
         }
-    }
 
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance() //Валидные значения кредита, больше нуля
+        { 
+            //Arrage
+            double beginningBalance = 0;
+            double creditAmount = 500;
+            double expected = 500;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            //Act
+            account.Credit(creditAmount);
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.0, "Account not credited correctly");
+        }
+
+        [TestMethod]
+        public void Credit_WhenCreditEqualZero_UpdatesBalance() //Валидные значения кредита, равен нулю
+        {
+            //Arrage
+            double beginningBalance = 500;
+            double creditAmount = 0;
+            double expected = 500;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            //Act
+            account.Credit(creditAmount);
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.0, "Account not credited correctly");
+        }
+
+        [TestMethod]
+        public void Credit_WhenAmountIsLessThanZero_SholdThrowArgumentOutOfRangeException() { //Кредит меньше нуля
+            //Arrage
+            double beginningBalance = 0;
+            double creditAmount = -1;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            //Act and Assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() =>
+             account.Credit(creditAmount));
+        }
+    }
 }
